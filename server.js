f760d3f57463d6dff8ca1aa0e4c527d477d8baa9
Kobi Gurkan <kobigurk@gmail.com>
2016-02-07 23:38:50 -0800

    server first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+var restify = require('restify');
+var fs = require('fs'); 
+var https = require('https');
+var http = require('http');
+var nodemailer = require('nodemailer');
+
+var server;
+var UserInfo = mongoose.model('user_info', {
+    email: String,
+    datetime: Date,
+    name: String,
+    amount: String
+});
+
+server = restify.createServer({
+    name: 'rusted',
+    version: '0.1.0'
+});
+server.use(restify.queryParser());
+server.use(restify.bodyParser({ mapParams: false}));
+server.use(
+    function crossOrigin(req,res,next){
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+          return next();
+    }
+);
+
+
+server.get('/', function (req, res, next) {
+    res.send('OK!!');
+});
+
+
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'rnd@magicinbits.com',
+        pass: 'dS$HVF123A1'
+    }
+});
+
+var port = 80;
+server.listen(port, '0.0.0.0', function () {
+    console.log('listening on port ' + port);
+});
+
